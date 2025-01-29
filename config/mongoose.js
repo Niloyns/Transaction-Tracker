d26c9b1ecf054011051beec25cb1efefdbf3412b
NiloyNs <niloysarkar1998@gmail.com>
2025-01-29 20:35:08 +0530

    d

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,9 +1,19 @@
+// const mongoose = require("mongoose");
+
+// mongoose.connect("mongodb+srv://niloysarkar1998:DPCGpwrxxP95yKdB@cluster0.z97yd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+// .then(()=>{
+//     console.log("success mongodb connection");
+// })
+// .catch((err)=>{
+//     console.log(err);
+// })
+
+
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://niloysarkar1998:DPCGpwrxxP95yKdB@cluster0.z97yd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-.then(()=>{
-    console.log("success mongodb connection");
+mongoose.connect("mongodb+srv://niloysarkar1998:DPCGpwrxxP95yKdB@cluster0.z97yd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 })
-.catch((err)=>{
-    console.log(err);
-})
+.then(() => console.log("✅ MongoDB Connected Successfully"))
+.catch((err) => console.error("❌ MongoDB Connection Failed:", err));
