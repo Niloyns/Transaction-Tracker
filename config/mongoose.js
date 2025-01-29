8575234287c443d3a6134faa67ef0b55339711f6
NiloyNs <niloysarkar1998@gmail.com>
2025-01-29 20:24:07 +0530

    update db

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://niloysarkar1998:<DPCGpwrxxP95yKdB>@cluster0.z97yd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect("mongodb+srv://niloysarkar1998:DPCGpwrxxP95yKdB@cluster0.z97yd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 .then(()=>{
     console.log("success mongodb connection");
 })
